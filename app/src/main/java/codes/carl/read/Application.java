2860d826e95203ae61e903866bd5359794dbb0f0
Carl Poole <mail@carlpoole.com>
2015-11-01 01:53:47 -0400

    First commit

---
+++ app/src/main/java/codes/carl/read/Application.java
@@ -0,0 +1,77 @@
+package codes.carl.read;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import net.dean.jraw.RedditClient;
+import net.dean.jraw.http.UserAgent;
+import net.dean.jraw.http.oauth.Credentials;
+import net.dean.jraw.http.oauth.OAuthData;
+import net.dean.jraw.http.oauth.OAuthException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.UUID;
+
+/**
+ * Created by carl on 10/31/15.
+ */
+public class Application extends android.app.Application {
+
+    public static RedditClient reddit;
+    public static Credentials credentials;
+
+    public static void authenticate() {
+        new Initiate().execute();
+    }
+
+    private static Context mContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mContext = this;
+    }
+
+    private static class Initiate extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            OAuthData authData;
+            try {
+                Properties props = new Properties();
+                props.load(mContext.getAssets().open("reddit.properties"));
+
+                reddit = new RedditClient(UserAgent.of(
+                        props.getProperty("platform"),
+                        props.getProperty("appID"),
+                        props.getProperty("version"),
+                        props.getProperty("name")));
+
+                credentials = Credentials.userlessApp(
+                        props.getProperty("clientID"),
+                        UUID.randomUUID());
+
+                authData = reddit.getOAuthHelper().easyAuth(credentials);
+                reddit.authenticate(authData);
+                return "";
+            } catch (OAuthException e) {
+                e.printStackTrace();
+                return null;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String account) {
+            if (account == null)
+                Log.e("READ", "AUTHENTICATION ERROR");
+        }
+    }
+
+}
---
+++ app/src/main/java/codes/carl/read/List.java
@@ -0,0 +1,130 @@
+package codes.carl.read;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import net.dean.jraw.RedditClient;
+import net.dean.jraw.http.NetworkException;
+import net.dean.jraw.http.UserAgent;
+import net.dean.jraw.http.oauth.Credentials;
+import net.dean.jraw.http.oauth.OAuthData;
+import net.dean.jraw.http.oauth.OAuthException;
+import net.dean.jraw.http.oauth.OAuthHelper;
+import net.dean.jraw.models.Listing;
+import net.dean.jraw.models.LoggedInAccount;
+import net.dean.jraw.models.Submission;
+import net.dean.jraw.paginators.SubredditPaginator;
+
+import java.net.URL;
+
+public class List extends AppCompatActivity {
+
+    RedditClient reddit;
+    SubredditPaginator frontPage;
+    ListView posts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        posts = (ListView) findViewById(R.id.posts);
+        Application.authenticate();
+
+        new GetSubmissions().execute();
+
+    }
+
+    private class GetSubmissions extends AsyncTask<String, Void, Listing<Submission>>{
+
+        @Override
+        protected Listing<Submission> doInBackground(String... params) {
+
+            while(!Application.reddit.isAuthenticated()){
+                System.out.println("Wait");
+            }
+
+            reddit = Application.reddit;
+            frontPage = new SubredditPaginator(reddit);
+            return frontPage.next();
+        }
+
+        @Override
+        protected void onPostExecute(Listing<Submission> subs) {
+            posts.setAdapter(new SubmissionAdapter(List.this,subs));
+        }
+    }
+
+    private class SubmissionAdapter extends ArrayAdapter{
+        private LayoutInflater inflater;
+
+        public SubmissionAdapter(Activity activity, Listing<Submission> submissions){
+            super(activity,R.layout.row,submissions);
+            inflater = activity.getWindow().getLayoutInflater();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            View rowView;
+            Submission sub;
+
+            if(convertView == null){
+                rowView = inflater.inflate(R.layout.row, parent, false);
+            }else{
+                rowView = convertView;
+            }
+
+            sub = (Submission)getItem(position);
+
+            TextView title = (TextView)rowView.findViewById(R.id.title);
+            TextView upvotes = (TextView)rowView.findViewById(R.id.upvotes);
+            title.setText(sub.getTitle());
+            upvotes.setText(String.valueOf(sub.getScore()));
+            return rowView;
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
